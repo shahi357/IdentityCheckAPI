665d5e3ba5720d51f965885e025301e51a78c9ee
Tanka Shahi <shahitank1@gmail.com>
2024-05-01 09:46:25 +1000

    server run code written

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./index");
+
+const port = process.env.PORT || 3000;
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
